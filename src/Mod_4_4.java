3de1344541c34f9cba96c4fce198237bc874dec9
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-06 13:49:32 -0500

    bal

---
+++ src/Mod_4_4.java
@@ -0,0 +1,25 @@
+public class Mod_4_4 {
+
+
+    public static void main(String[] args) {
+
+        int initialBalance = 1000;
+        double Interest = 1.05;
+        double yearOne = initialBalance * Interest;
+        double yearTwo = yearOne * Interest;
+        double yearThree = yearTwo * Interest;
+
+
+        System.out.println("Year one is" + yearOne);
+        System.out.println("Year two is" + yearTwo);
+        System.out.println("Year three is" + yearThree);
+
+
+
+
+    }
+
+
+
+
+}
