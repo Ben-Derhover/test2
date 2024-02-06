e380eb9db4aef4bcedeba8699add602395808ed7
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-06 13:55:11 -0500

    name

---
+++ src/Mod_4_P5.java
@@ -0,0 +1,20 @@
+public class Mod_4_P5 {
+
+    public static void main(String[] args){
+
+        System.out.println("   *******************");
+        System.out.println("  *******         *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println(" *******           *******");
+        System.out.println("  *******         *******");
+        System.out.println("   *******************");
+
+
+    }
+}
