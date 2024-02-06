340ed846f742b76f2f7cc6b803bbc5611be6b02e
Ben-Derhover <Oliver.james@talawanda.org>
2024-02-06 13:41:02 -0500

    ball

---
+++ src/Mod_4_3.java
@@ -0,0 +1,15 @@
+public class Mod_4_3 {
+
+    public static void main(String[] args) {
+
+        int myTotal = 0;
+
+        myTotal = 2 + 4 + 6 + 8 + 10;
+        System.out.println("the total is"+ myTotal );
+
+
+
+
+
+    }
+}
